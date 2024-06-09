66dc6bea912243de329372f0f108e2e415ac9ae7
Marcus Plutowski <achierius@gmail.com>
2024-06-09 14:40:49 -0700

    add script to install dotfiles
    I could use stow but it's not clear that the (minor) ease of use is
    worth adding another package to my base system everywhere.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Works as long as the last component is 
+cwd=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+files_installed_in_homedir=(".bash_profile" ".bashrc" ".gitconfig" ".tmux.conf" ".vimrc" ".zprofile" ".zshenv" ".zshrc")
+dirs_installed_in_config=("nvim")
+
+for file in ${files_installed_in_homedir[@]}; do
+	ln -si $cwd/$file ~/$file
+done
+
+for dir in ${dirs_installed_in_config[@]}; do
+	ln -si $cwd/config/$dir/ -t ~/.config/
+done
