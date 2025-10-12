b34db3654e16b985c43b5c7f7924c5a30bf0e1ab
Marcus Plutowski <achierius@gmail.com>
2025-10-12 11:20:21 -0700

    setup: add glances, lnav to PKGS_MONITORING
    glances: fuller btop alternative with web-server capabilities
    lnav: log viewer

--- system_setup.sh
+++ system_setup.sh
@@ -13,7 +13,7 @@ mkdir -p "$HOME/Documents"
 # Package lists
 PKGS_CORE="zsh git git-delta bat eza fzf vim neovim btop tree stow ripgrep rsync tmux fd-find direnv moreutils nnn jq xq yq"
 PKGS_INFRA="restic pass wireguard-tools"
-PKGS_MONITORING="cockpit vmstat iostat netstat sysstat vnstat lm_sensors"
+PKGS_MONITORING="cockpit vmstat iostat netstat sysstat vnstat lm_sensors glances lnav"
 # Mostly from https://www.brendangregg.com/blog/2024-03-24/linux-crisis-tools.html
 PKGS_DEBUG="procps-ng util-linux sysstat iproute numactl tcpdump kernel-tools perf bcc-tools bpftrace trace-cmd nicstat ethtool tiptop cpuid msr-tools"
 PKGS_DESKTOP="flatpak firefox feh ncdu mpv ffmpeg sqlitebrowser wireshark openjdk rofi pandoc poppler-utils"
